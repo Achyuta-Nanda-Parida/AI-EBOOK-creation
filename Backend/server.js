b9eb7b2610059d41ed8f0cf233794e3020ddddeb
achyuta <achyutanandaparida65@gmail.com>
2025-11-05 22:36:02 +0530

    add 8000 port

--- Backend/server.js
+++ Backend/server.js
@@ -56,7 +56,7 @@ app.use("/api/ai", aiRoutes);
 app.use("/api/export", exportRoutes);
 
 
-const PORT = process.env.PORT || 5000;
+const PORT = process.env.PORT || 8000;
 app.listen(PORT, ()=>{
     console.log(`Server running on port ${PORT}`);
 });
