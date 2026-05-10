b0658f6d5bd14235935e1b4a31d172ae0dd74a68
achyuta <achyutanandaparida65@gmail.com>
2026-05-10 12:21:55 +0530

    code fixed

--- Backend/server.js
+++ Backend/server.js
@@ -2,6 +2,7 @@ require("dotenv").config();
 const express = require("express");
 const cors = require("cors");
 const path = require("path");
+
 const connectDB = require("./config/db");
 const authRoutes = require("./routes/authRoutes");
 const bookRoutes = require("./routes/bookRoutes");
@@ -9,55 +10,60 @@ const aiRoutes = require("./routes/aiRoutes");
 const exportRoutes = require("./routes/exportRoutes");
 
 const app = express();
-// const allowedOrigins=[
-//   "https://ai-ebook-creation.vercel.app",
-//   "http://localhost:5173"
-// ]
-
-// app.use(
-//   cors({
-//      origin:allowedOrigins,
-//     credentials: true,
-//     methods: ["GET", "POST", "PUT", "DELETE"],
-//     allowedHeaders: ["Content-Type", "Authorization"],
-//   })
-// );
-
 const allowedOrigins = [
-  "ai-ebook-creation.vercel.app",
+  "https://ai-ebook-creation.vercel.app",
+  "https://ai-ebook-creation-git-main-achyuta-nanda-paridas-projects.vercel.app",
+  "https://ai-ebook-creation-ba69g0x37-achyuta-nanda-paridas-projects.vercel.app",
   "http://localhost:5173",
 ];
 
 app.use(
   cors({
     origin: function (origin, callback) {
-      if (!origin || allowedOrigins.includes(origin)) {
-        callback(null, true);
-      } else {
-        callback(new Error("Not allowed by CORS"));
+      console.log("Request Origin:", origin);
+      if (!origin) {
+        return callback(null, true);
       }
+      if (allowedOrigins.includes(origin)) {
+        return callback(null, true);
+      }
+
+      // Reject others
+      return callback(new Error("Not allowed by CORS"));
     },
+
     credentials: true,
-    methods: ["GET", "POST", "PUT", "DELETE"],
+    methods: ["GET", "POST", "PUT", "DELETE", "OPTIONS"],
     allowedHeaders: ["Content-Type", "Authorization"],
   })
 );
-
-
 connectDB();
 
 app.use(express.json());
-app.use(express.urlencoded({extended:true}))
+app.use(express.urlencoded({ extended: true }));
 
-app.use("/backend/uploads", express.static(path.join(__dirname, "uploads")));
+app.use(
+  "/backend/uploads",
+  express.static(path.join(__dirname, "uploads"))
+);
+
 
 app.use("/api/auth", authRoutes);
 app.use("/api/books", bookRoutes);
 app.use("/api/ai", aiRoutes);
 app.use("/api/export", exportRoutes);
 
+app.use((err, req, res, next) => {
+  console.error("Server Error:", err.message);
+
+  res.status(500).json({
+    success: false,
+    message: err.message || "Internal Server Error",
+  });
+});
 
 const PORT = process.env.PORT || 8000;
-app.listen(PORT, ()=>{
-    console.log(`Server running on port ${PORT}`);
+
+app.listen(PORT, () => {
+  console.log(`Server running on port ${PORT}`);
 });
